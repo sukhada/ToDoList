2c8434502ea1e4044fc66882f21f38ccde328018
Sukhada Kulkarni <skulkarni@hearsaycorp.com>
2016-07-24 22:34:07 -0700

    updates to layout view

--- app/src/main/java/com/example/skulkarni/todolist/ToDoItemAdapter.java
+++ app/src/main/java/com/example/skulkarni/todolist/ToDoItemAdapter.java
@@ -24,20 +24,15 @@ public class ToDoItemAdapter extends ArrayAdapter<ToDoItem> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // Get the data item for this position
         ToDoItem toDoItem = getItem(position);
-        // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.todo_item, parent, false);
         }
-        // Lookup view for data population
         TextView editItem = (TextView) convertView.findViewById(R.id.todo_item_edit_text);
         TextView dueDate = (TextView) convertView.findViewById(R.id.todo_item_due_date);
         DateFormat df = new SimpleDateFormat("EEE MMM d yyyy");
-        // Populate the data into the template view using the data object
         editItem.setText(toDoItem.text);
         dueDate.setText(df.format(toDoItem.dueDate).toString());
-        // Return the completed view to render on screen
         return convertView;
     }
 
